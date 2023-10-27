d438d8332812762e2439e9ee5fb5c9d04f36eaaa
Ouyahia Sara-lyna <saralyna.ouyahia.etu@a13p11.fil.univ-lille.fr>
2023-10-27 09:46:44 +0200

    question 1 et 2 finies

--- tp1/mainWindow.py
+++ tp1/mainWindow.py
@@ -1,8 +1,27 @@
 import sys
+from PyQt5.QtWidgets import *
+
+
+class MainWindow(QMainWindow):
+
+	def __init__(self):
+		super().__init__()
+
+
+def main(args):
+	print(args)
+	app = QApplication(args)
+	window= MainWindow()
+	window.show()
+	app.exec_()
+
+
+
+if __name__ == "__main__":
+	print("Exécution du programme")
+	main(sys.argv)
+
 
 
 
 	
-
-if __name__ == "__main__":
-	print("execution du programme")
