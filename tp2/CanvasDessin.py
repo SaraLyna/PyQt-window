2ffc33eb866feb01d27ad34fd7de5c089802aa5e
Sara Lyna OUYAHIA <saralyna2002@gmail.com>
2023-11-11 22:40:02 +0100

    exercice 2 bientot fini

--- tp2/CanvasDessin.py
+++ tp2/CanvasDessin.py
@@ -12,35 +12,55 @@ class CanvasDessin(QWidget):
 		super().__init__()
 		self.setMinimumSize(800,600)
 		self.traces = []
+		self.current_trace = None
 
 
 
 	def paintEvent(self, event):
 		
 		painter = QPainter(self)
-		path = QPainterPath()
-		for trace in self.traces :
-			path.setBrush(Qt.red)
 
-		painter.drawPath(path)
+		for trace in self.traces :
+
+			path = QPainterPath()
+			pen = QPen(trace.color, trace.width,cap=Qt.RoundCap, join=Qt.RoundJoin )
+			painter.setPen(pen)
+			path.moveTo(trace.points[0])
+
+			for point in trace.points[1:]:
+				path.lineTo(point)
+
+			painter.drawPath(path)
+
+		if self.current_trace:
+
+			path2 = QPainterPath()
+            pen2 = QPen(self.current_trace.color, self.current_trace.width, cap=Qt.RoundCap, join=Qt.RoundJoin)
+            painter.setPen(pen2)
+            path2.moveTo(self.current_trace.points[0])
+
+            for point in self.current_trace.points[1:]:
+                path2.lineTo(point)
+
+            painter.drawPath(path2)
 
 
 	def press(self, event):
-
-
+		self.current_trace = Trace([event.pos()], 3, Qt.red)
 		self.update()
 
 
 	def drag(self, event):
-
-
-		self.update()
+		if self.current_trace:
+			self.current_trace.points.append(event.pos())
+			self.update()
 
 
 	def release(self, event):
-
-
-		self.update()
+		if self.current_trace:
+			self.traces.append(self.current_trace)
+			self.current_trace = None
+			self.update()
 
 
 
